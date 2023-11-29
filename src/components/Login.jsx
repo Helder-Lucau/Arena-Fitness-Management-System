ad0ec3aa1d1d1db4b997e836759c1d528febf551
Helder-Lucau <helderlucau@gmail.com>
2023-11-29 09:06:00 +0300

    login button styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,20 +4,22 @@ const Login = () => {
   return (
     <div>
       <div className="relative w-full h-screen bg-zinc-900/70">
+        {/*Background Image*/}
         <img src={Image} alt="/" className="absolute w-full h-full object-cover mix-blend-overlay" />
         <div className="flex justify-center items-center h-full relative">
+          {/*Form */}
           <form className='max-w-[400px] w-full mx-auto bg-white p-8 rounded-lg'>
             <h2 className="text-3xl font-bold text-orange-500 text-center tracking-wide py-6">The Arena Fitness Center</h2>
             <p className='font-light text-gray-400 mb-8 text-center'>Welcome to Arena Fitness Center Management System</p>
             <div className='flex flex-col py-2'>
-              <label className="">Username</label>
-              <input type="text" placeholder="Enter your username" className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" />
+              <label>Username</label>
+              <input type="text" placeholder="Enter your username" className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500 mt-1" />
             </div>
             <div className='flex flex-col py-2'>
-              <label className="">Password</label>
-              <input type="password" placeholder="Enter your password" className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" />
+              <label>Password</label>
+              <input type="password" placeholder="Enter your password" className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500 mt-1" />
             </div>
-            <button className="border w-full text-white my-5 p-2 rounded-2xl bg-orange-500 hover:bg-orange-200">Log in</button>
+            <button className="border w-full text-white my-5 p-2 rounded-2xl bg-orange-500 hover:bg-orange-400 hover:scale-105 duration">Log in</button>
             <button className="border w-full p-2 rounded-2xl border-gray-300 hover:bg-gray-200">Forgot password?</button>
           </form>
         </div>
